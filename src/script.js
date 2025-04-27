2b6b07f7fbb34930b74523ebc439db24fadf88fd
nixxinix <kingsamarita879@gmail.com>
2025-04-27 19:14:00 +0800

    Created  my portfolio repositories.

---
+++ src/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Computer Engineering Student", "Student", "Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
